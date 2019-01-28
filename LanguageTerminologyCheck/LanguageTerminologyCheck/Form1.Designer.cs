c5af7d485216571c83e454cd3c86c212944e7de2
ashumeow <ashumeow@live.com>
2019-01-28 21:01:51 +0300

    update

---
+++ LanguageTerminologyCheck/LanguageTerminologyCheck/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace LanguageTerminologyCheck
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(203, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(204, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "TEST";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 229);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(950, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Once the \"TEST\" button is clicked, then it must translate \"hello, how are you?\" f" +
+    "rom \"en-US\" (English) to \"es-ES\" (Spanish) as displayed in pop-up box";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1047, 295);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ LanguageTerminologyCheck/LanguageTerminologyCheck/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+using LanguageTerminologyCheck.TerminologyService;
+
+namespace LanguageTerminologyCheck
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Create a collection to define the desired sources of translations             
+            TranslationSources translationSources = new TranslationSources() {
+                TranslationSource.Terms, TranslationSource.UiStrings
+            };
+            // Create the proxy for the Terminology Service SOAP client
+            TerminologyClient service = new TerminologyService.TerminologyClient();
+            // Call GetTranslations to get the results
+            Matches results = service.GetTranslations("hello, how are you?", "en-US", "es-ES", SearchOperator.Contains, translationSources, false, 20, true, null);
+            // Use the results
+            foreach (Match match in results)
+            {
+                MessageBox.Show(match.OriginalText + "----" + match.Translations[0].TranslatedText);
+            }
+        }
+    }
+}
+// Aswini S
+
+// https://www.microsoft.com/en-us/language/Microsoft-Terminology-API
